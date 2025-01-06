1707a3f548b86111a3716da408df319fa895e28c
RobertRosu <i_rosu@fpzornotza.com>
2025-01-06 13:17:57 +0100

    Clase  creado

---
+++ src/app/model/Articulo.ts
@@ -0,0 +1,9 @@
+export class Articulo{
+    constructor(
+        public id: number,
+        public nombre: string,
+        public descripcion: string,
+        public precio: number,
+        public imagen: string
+    ){}
+}
