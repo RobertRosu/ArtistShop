a3216af73c4161588476d03894d27e85d8a05d38
RobertRosu <i_rosu@fpzornotza.com>
2025-01-06 16:36:27 +0100

    tabla con datos terminada

--- src/app/components/listado/listado.component.ts
+++ src/app/components/listado/listado.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Articulo } from 'src/app/model/Articulo';
+import { ArticulosService } from 'src/app/services/articulos.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.scss']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit{
+  articulos!: Articulo[]
+  constructor(private _articulosService: ArticulosService){}
 
+  ngOnInit(): void {
+    this._articulosService.recuperarTodos().subscribe((data: Articulo[]) => {
+      this.articulos = data
+      console.log(data)
+    })
+  }
+
+  getId(item: any): number {
+    return item.id; // Usa el ID como identificador único
+  }
 }
